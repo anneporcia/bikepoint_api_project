098129944d791a23965fc6b30b3a2b4dace22c05
anneporcia <annporcia@hotmail.com>
2026-01-12 11:09:30 +0000

    Added load script

---
+++ load_bike_point.py
@@ -0,0 +1,44 @@
+# install necessary packages for loading to 
+import boto3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# variables for aws credentials 
+aws_access_key = os.getenv('AWS_ACCESS_KEY')
+aws_secret_key = os.getenv('AWS_SECRET_KEY')
+bucket_name = os.getenv('bucket_name')
+
+filepath = 'data'
+
+# config to upload to the s3 bucket
+s3_client = boto3.client(
+            's3'
+            ,aws_access_key_id = aws_access_key
+            ,aws_secret_access_key = aws_secret_key
+        )
+
+# for loop to go through all the files in the data folder
+for file in os.listdir(filepath):
+        # only uploading .json files
+        if not file.endswith('.json'):
+                continue
+        
+        # creates full filepath for .upload_file function
+        local_file_path = os.path.join(filepath, file)
+        
+        # s3_filename = '2026-01-07 15-28-59.json'
+        try:
+            #uploads file from local directory to bucket
+            s3_client.upload_file(local_file_path, bucket_name, file)
+            # removes the file from local directory
+            os.remove(local_file_path)
+
+        except Exception as e:
+            print(f'Upload error for {file}: {e}')
+
+
+
+
+
